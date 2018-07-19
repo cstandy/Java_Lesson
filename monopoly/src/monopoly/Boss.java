f8389b2d57f5cfc9e05bff995feee6a326f3b607
cstandy <cst.andy@gmail.com>
2018-07-19 16:07:59 +0800

    Update Boss.java for comment

--- monopoly/src/monopoly/Boss.java
+++ monopoly/src/monopoly/Boss.java
@@ -1,13 +1,20 @@
 package monopoly;
 
 public class Boss {
+
 	/* Variable */
-	private int order;			//順序
-	private int cost;			//參加打鬥花費
-	private int point;			//獲得分數
-	private int requirement;	//至少要擲到幾分才能贏
+	private int order;			// 順序
+	private int cost;			// 參加打鬥花費
+	private int point;			// 獲得分數
+	private int requirement;	// 至少要擲到幾分才能贏
 	
-	/* Constructor */
+	/**
+	  * @brief              The constructor of the class.
+	  * @parame order       The order of the boss.
+	  * @parame cost        The attending fee of player.
+	  * @parame point       The prize of point.
+	  * @parame requirement The low bound to win this boss.
+	  */
 	public Boss(int order, int cost, int point, int requirement) {
 		this.order = order;
 		this.cost = cost;
